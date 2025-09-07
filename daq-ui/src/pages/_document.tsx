0f334774f070986f90c6ab3ecb256687af05acd0
Paul Mikol <paul.mikol@gmail.com>
2025-09-06 22:42:38 -0500

    m

--- daq-ui/src/pages/_document.tsx
+++ daq-ui/src/pages/_document.tsx
@@ -2,20 +2,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
-    const aiOrigin = process.env.NEXT_PUBLIC_AI_UI_ORIGIN || "https://mesh-daq.fullstackjedi.dev/ai-demo";
-    return (
+      return (
         <Html lang="en">
             <Head />
             <body>
             <Main />
             <NextScript />
             <Script
-                src={`${aiOrigin.replace(/\/$/, "")}/dock/boot.js`}
+                src={`${(process.env.NEXT_PUBLIC_AI_UI_ORIGIN || "https://mesh-daq.fullstackjedi.dev/ai-demo").replace(/\/$/, "")}/dock/boot.js`}
                 strategy="afterInteractive"
-                data-origin={aiOrigin}
+                data-origin={process.env.NEXT_PUBLIC_AI_UI_ORIGIN}
                 data-visible="1"
                 data-height="360"
-                crossOrigin="anonymous"
             />
             </body>
         </Html>
