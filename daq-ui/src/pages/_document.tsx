55aff38f9aa5bbc042bbcbedbbca1807ae6f6f28
Paul Mikol <paul.mikol@gmail.com>
2025-09-07 02:51:23 -0500

    m

--- daq-ui/src/pages/_document.tsx
+++ daq-ui/src/pages/_document.tsx
@@ -1,19 +1,20 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
-    const ORIGIN = (process.env.NEXT_PUBLIC_AI_UI_ORIGIN || "").replace(/\/$/, "");
     return (
         <Html lang="en">
             <Head />
             <body>
             <Main />
             <NextScript />
+
+            {/* Use Next.js Script with a loading strategy */}
             <Script
-                id="dock-boot"
-                src={`${ORIGIN}/dock/boot.js`}
+                src="https://ai-ui.fullstackjedi.dev/dock/boot.js"
                 strategy="afterInteractive"
-                data-origin={ORIGIN}
+                data-origin="https://ai-ui.fullstackjedi.dev"
                 data-visible="1"
                 data-height="360"
             />
