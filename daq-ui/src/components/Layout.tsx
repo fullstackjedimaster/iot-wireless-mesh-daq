e46a812e3aa23be2a701f036e7d88b4da5303e5c
Paul Mikol <paul.mikol@gmail.com>
2025-09-10 23:22:24 -0500

    m

--- daq-ui/src/components/Layout.tsx
+++ daq-ui/src/components/Layout.tsx
@@ -1,14 +1,14 @@
 // daq-ui/src/components/Layout.tsx
 import React from "react";
 import Head from "next/head";
-import EmbedHeightReporter from "@/components/EmbedHeightReporter";
+// import EmbedHeightReporter from "@/components/EmbedHeightReporter";
 import "@/app/globals.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Head><meta name="viewport" content="width=device-width, initial-scale=1" /></Head>
-            <EmbedHeightReporter />
+            {/*<EmbedHeightReporter />*/}
             <main className="main-section">{children}</main>
         </>
     );
