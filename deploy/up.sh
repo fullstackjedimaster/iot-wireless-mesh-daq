5ab89597ff6364e476e9ed7f214ae27f6428b5a0
Paul Mikol <paul.mikol@gmail.com>
2026-05-27 20:11:53 -0500

    auto: sync from droplet

--- deploy/up.sh
+++ deploy/up.sh
@@ -1,26 +1,43 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Resolve deploy dir from this script's location
 DEPLOY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# hard pin compose identity
 COMPOSE_PROJECT_NAME="$(basename "$(dirname "$DEPLOY_DIR")")"
+
+PORTFOLIO_DEPLOY="/opt/stacks/portfolio/deploy"
 
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
 
+docker compose \
+  -p "$COMPOSE_PROJECT_NAME" \
+  -f "$DEPLOY_DIR"/compose.yml \
+  down -v --remove-orphans
 
-docker compose -p "$COMPOSE_PROJECT_NAME" -f "$DEPLOY_DIR"/compose.yml down -v --remove-orphans
+echo "[up] Initializing shared env"
+bash "$PORTFOLIO_DEPLOY/shared/scripts/init-shared-env.sh"
 
 echo "[up] Generating env + secrets"
-bash -x "$DEPLOY_DIR/scripts/init-env.sh"
+bash "$DEPLOY_DIR/scripts/init-env.sh"
 
-#docker compose -p "$COMPOSE_PROJECT_NAME" -f "$DEPLOY_DIR"/compose.yml  build --no-cache
+echo "[up] Loading env"
+
+set -a
+
+source "$PORTFOLIO_DEPLOY/shared/env/embed.env"
+source "$DEPLOY_DIR/env/cloud.env"
+source "$DEPLOY_DIR/env/daq-ui.env"
+
+set +a
 
 echo "[up] Building + starting stack"
-#docker compose -p "$COMPOSE_PROJECT_NAME" -f "$DEPLOY_DIR"/compose.yml  up -d --force-recreate --renew-anon-volumes --remove-orphans
-docker compose -p "$COMPOSE_PROJECT_NAME" -f "$DEPLOY_DIR"/compose.yml  up -d --build
+
+docker compose \
+  -p "$COMPOSE_PROJECT_NAME" \
+  -f "$DEPLOY_DIR"/compose.yml \
+  up -d --build
+
 echo "[up] Done"
 
 docker compose logs -f daq-ui cloud
