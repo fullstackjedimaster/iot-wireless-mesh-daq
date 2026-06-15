ac46242bbf5a04279323fb218fd7153bca4e1375
Paul Mikol <paul.mikol@gmail.com>
2026-06-14 19:24:35 -0500

    auto: sync from droplet

--- daq-ui/src/lib/settings.ts
+++ daq-ui/src/lib/settings.ts
@@ -30,8 +30,8 @@ export const settings = {
      process.env.NEXT_PUBLIC_DOCK_FRAME_ID ||
       "daq-dock",
 
-  PORTFOLIO_LOCK_ENABLED:
-      process.env.PORTFOLIO_LOCK_ENABLED ||
+  EMBED_LOCK_ENABLED:
+      process.env.EMBED_LOCK_ENABLED ||
       "true",
 
   EMBED_SECRET:
